c95560d7d33bc25ee7c21885a9490159f16b10ad
Massil <yatagh_m@epitech.eu>
2015-03-17 05:59:43 +0800

    added documentation for UITimeBlockArray

--- Orphee/UITimeBlockArray.swift
+++ Orphee/UITimeBlockArray.swift
@@ -8,10 +8,14 @@
 
 import UIKit
 
+/// This class is a wrapper around a `UITrackTimeBlock` array.
+/// It provides
 class UITimeBlockArray {
 
+    /// The actual array of `UITrackTimeBlocks`
     private var buttons: [UITrackTimeBlock?] = [];
 
+    /// The row this array represents in the UI.
     var row: Int = 0 {
         didSet(newValue) {
             for button in self.buttons {
@@ -20,8 +24,11 @@ class UITimeBlockArray {
         }
     }
 
+    /// The number of `UITrackTimeBlocks` this array contains.
     private var size: Int = 0;
 
+    /// The position of the furthest point represented by this array.
+    /// The value is calculated when required based on the `size`, the `row` number and the size of the contained buttons.
     var endPos: (x: Int, y: Int) {
         get {
             var x: Int = size * (UITrackTimeBlock.width + 10) + 10;
@@ -30,12 +37,25 @@ class UITimeBlockArray {
         }
     }
 
+    /// MARK: Init
+    //
 
+    /// Init
+    ///
+    /// :param: rowNbr  The row number represented by this array.
     init(rowNbr: Int) {
 
         row = rowNbr;
     }
 
+    /// MARK: Add/Remove buttons
+    //
+
+    /// Adds `count` buttons to this array and sets them as subviews to a given view.
+    ///
+    /// :param: count   The number of buttons to add to the array and given view.
+    /// :param: color   The buttons' color.
+    /// :param: toView  The view to which to add these buttons.
     func addButtons(count: Int, color: UIColor, toView view: UIView) {
 
         for (var col = 0; col < count; col++) {
@@ -47,6 +67,10 @@ class UITimeBlockArray {
         size = buttons.count;
     }
 
+    /// Removes the `count` last buttons contained in this array from a given view.
+    ///
+    /// :param: count       The number of buttons to remove from the array and given view.
+    /// :param: fromView    The view from which to remove these buttons.
     func removeButtons(count: Int, fromView view: UIView) {
 
         let safeCount = (count <= size) ? count : size;
