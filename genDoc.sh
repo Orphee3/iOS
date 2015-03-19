de403f62dca56f1ab1267eb138f0805824cc82c7
Massil <yatagh_m@epitech.eu>
2015-03-19 18:26:18 +0800

    added script to generate documentation using 'jazzy'
          . jazzy is a ruby package (or ruby-gem). To install, type: `sudo gem install jazzy`
          . If this does not work, try installing another version of ruby with `rbenv` from github.

---
+++ genDoc.sh
@@ -0,0 +1,14 @@
+NORM="\033[0m"
+GREEN="\033[32m"
+
+echo -e "generating documentation...\n"
+jazzy -c -a "Massil Yataghene, Jeromin Lebon" -g https://github.com/Orphee3/iOS;
+echo -e "\t\t\t\t$GREEN OK $NORM"
+
+echo -en "\ncopying documentation...\t"
+cp -rf docs/* ~/Desktop/orpheeDoc;
+echo -e "$GREEN OK $NORM"
+
+echo -ne "cleaning...\t\t\t"
+rm -rf build docs;
+echo -e "$GREEN OK $NORM\n"
