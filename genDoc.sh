09b740e65b1c3a630464238f0b34983203358621
Massil <yatagh_m@epitech.eu>
2015-03-21 01:40:25 +0800

    modified doc generating script to fit project structure change

--- genDoc.sh
+++ genDoc.sh
@@ -2,7 +2,7 @@ NORM="\033[0m"
 GREEN="\033[32m"
 
 echo -e "generating documentation...\n"
-jazzy -c -a "Massil Yataghene, Jeromin Lebon" -g https://github.com/Orphee3/iOS;
+jazzy -c -a "Massil Yataghene, Jeromin Lebon" -g https://github.com/Orphee3/iOS -x -workspace,Orphee.xcworkspace/,-scheme,Orphee;
 echo -e "\t\t\t\t$GREEN OK $NORM"
 
 echo -en "\ncopying documentation...\t"
