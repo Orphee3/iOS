c9a6536d322129713996f4c5c76fed62c81d072b
Massil <yatagh_m@epitech.eu>
2015-07-09 15:03:40 +0200

    modified genDoc script. Still need to add links to root index manually for each module

--- genDoc.sh
+++ genDoc.sh
@@ -1,14 +1,11 @@
 NORM="\033[0m"
 GREEN="\033[32m"
+DOCPATH="/Users/$USER/Desktop/orpheeDoc"
+SRCPATH="/Users/$USER/Desktop/Orphee"
 
-echo -e "generating documentation...\n"
-jazzy -c -a "Massil Yataghene, Jeromin Lebon" -g https://github.com/Orphee3/iOS -x -workspace,Orphee.xcworkspace/,-scheme,Orphee;
+echo -e "generating documentation for module: Orphee...\n"
+jazzy -o $DOCPATH/orphee -c -a "Massil Yataghene, Jeromin Lebon" -g https://github.com/Orphee3/iOS -x -workspace,Orphee.xcworkspace/,-scheme,Orphee --min-acl internal;
 echo -e "\t\t\t\t$GREEN OK $NORM"
-
-echo -en "\ncopying documentation...\t"
-cp -rf docs/* ~/Desktop/orpheeDoc;
-echo -e "$GREEN OK $NORM"
-
-echo -ne "cleaning...\t\t\t"
-rm -rf build docs;
-echo -e "$GREEN OK $NORM\n"
+echo -e "generating documentation for module: FileManagement...\n"
+jazzy -o $DOCPATH/filemgt -c -a "Massil Yataghene, Jeromin Lebon" -g https://github.com/Orphee3/iOS -x -workspace,Orphee.xcworkspace/,-scheme,FileManagement --min-acl internal -e $SRCPATH/FileManagement/File.swift,$SRCPATH/FileManagement/ByteBuffer.swift,$SRCPATH/FileManagement/ByteOrder.swift;
+echo -e "\t\t\t\t$GREEN OK $NORM"
